76edcf008bc568d173022a4b8a957b25db5eec2e
Santiago Giraldo M <santi_rm8@hotmail.com>
2016-10-22 17:03:48 -0500

    New style the view about, add images, change color navbar and footer

--- app/assets/javascripts/about/about.js
+++
@@ -1,8 +0,0 @@
-$(window).resize(function(){
-  if ($(window).width() <= 800 ){
-	  $(".card_class").removeClass("card horizontal").addClass("card");
-  }
-  if ($(window).width() > 800){
-	  $(".card_class").removeClass("card").addClass("card horizontal");
-  }
-});
