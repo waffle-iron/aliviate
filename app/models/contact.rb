07901aeaa471a0ce5ccee09b1a1c11321511081b
Mauricio Serna Flórez <mauricioserf@gmail.com>
2016-10-24 19:43:00 -0500

    Adding google map in contact section, adding margin-left to button-collapse, adding form for contact section

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact < MailForm::Base
 
-  attribute :name,      validate: true
-  attribute :message,     validate: true
-  attribute :email,   validate: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  attribute :full_name, validate: true
+  attribute :message,   validate: true
+  attribute :email,     validate: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   attribute :nickname,  captcha: true
 
   def headers
