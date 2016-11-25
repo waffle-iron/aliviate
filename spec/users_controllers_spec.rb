fbcf2b4d78c20fe88bf88d0841763bee975a8f0a
Miguel <mra634463@gmail.com>
2016-11-25 12:47:14 -0500

    Fixed erros rebase

---
+++ spec/users_controllers_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe UsersController, type: :controller do
+
+  let(:user) { create(:user) }
+
+  describe "Users Update" do
+    it "PUT update/:id" do
+      put :update, params: {id: user.id, user: {full_name: "Carlos"}}
+      user.reload
+      expect(user.full_name).to eq("Carlos")
+      expect(response).to redirect_to proc { edit_user_url(user) }
+    end
+  end
+
+  describe "PUT update/:id" do
+    it "returns http success" do
+      put :update, params: {id: user.id, user: {password: "123456"}}
+      user.reload
+      expect(user.password).tp eq("123456")
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, params: {id: user.id}
+      expect(response).to redirect_to proc {edit_user_url(user)}
+    end
+  end
+
+end
