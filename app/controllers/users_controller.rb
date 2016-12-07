087fe3ddcaae95f9823ba4fb3cc3548aa8c92873
Miguel <mra634463@gmail.com>
2016-12-07 13:05:39 -0500

    solved conflicts in the last commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,30 +3,12 @@ class UsersController < ApplicationController
   # Callbacks
 
   before_action :validate_user
-<<<<<<< HEAD
   before_action :edit, only: [:update, :edit]
-
-=======
->>>>>>> 5895a0334a419d985270980a7af326eff702c8cb
 
   # Methods
 
-  def show
-    @user = User.find(params[:id])
-  end
-
-<<<<<<< HEAD
   def update
     if @user.update(permit_params)
-=======
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(permit_params)
->>>>>>> 5895a0334a419d985270980a7af326eff702c8cb
       flash[:success] = t('successful_update')
       redirect_to edit_user_url(@user)
     else
@@ -37,13 +19,10 @@ class UsersController < ApplicationController
 
   private
 
-<<<<<<< HEAD
     def edit
       @user = User.find(params[:id])
     end
 
-=======
->>>>>>> 5895a0334a419d985270980a7af326eff702c8cb
     def permit_params
     params.require(:user).permit(:full_name, :email, :telephone,
                                  :cellphone, :address)
--- spec/controllers/users_controllers_spec.rb
+++ spec/controllers/users_controllers_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe UsersController, type: :controller do
 
   let(:user) { create(:user) }
-<<<<<<< HEAD
   before(:each) { sign_in(user) }
 
   describe "PUT update" do
@@ -13,36 +12,13 @@ RSpec.describe UsersController, type: :controller do
       expect(user.full_name).to eq("Alberto")
       expect(response).to redirect_to proc {edit_user_url(user)}
     end
+  end  
 
-=======
-
-  describe "Users Update" do
-    it "PUT update/:id" do
-      put :update, params: {id: user.id, user: {full_name: "Carlos"}}
-      user.reload
-      expect(user.full_name).eql?(:full_name)
-      expect(response).to redirect_to proc { edit_user_url(user) }
-    end
-  end
-
-  describe "PUT update/:id" do
-    it "returns http success" do
-      put :update, params: {id: user.id, user: {password: "123456"}}
-      user.reload
-      expect(user.password).eql?(:password)
-      expect(response).to redirect_to root_path
-    end
->>>>>>> 5895a0334a419d985270980a7af326eff702c8cb
-  end
 
   describe "GET edit" do
     it "returns http success" do
       get :edit, params: {id: user.id}
-<<<<<<< HEAD
       expect(response).to have_http_status(:success)
-=======
-      expect(response).to redirect_to proc {edit_user_url(user)}
->>>>>>> 5895a0334a419d985270980a7af326eff702c8cb
     end
   end
 
