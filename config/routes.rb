d93791323d770e3d94937ea2462cb2c4b33fa6c7
Miguel <mra634463@gmail.com>
2016-12-13 18:06:34 -0500

    Correct the edit and fuse with password update, also correct the test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks",
+                                    registrations: "users/registrations"}
 
+  # devise_for :users, controllers: { registrations: "users/registrations" }
   namespace :admin do
     root to: "dashboards#index"
     resources :posts, except: [:show]
--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,18 +1,22 @@
 require "rails_helper"
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe RegistrationsController, type: :controller do
 
   let(:user) { create(:user) }
   before(:each) { sign_in(user) }
+
+  before :each do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+  end
 
   describe "PUT update" do
     it "updates user information" do
       put :update, params: {id: user.id, user: {full_name: "Alberto"}}
       user.reload
       expect(user.full_name).to eq("Alberto")
-      expect(response).to redirect_to proc {edit_user_url(user)}
+      expect(response).to redirect_to root_path
     end
-  end  
+  end
 
 
   describe "GET edit" do
