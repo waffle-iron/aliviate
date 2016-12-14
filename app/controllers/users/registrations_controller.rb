48afd2e3c35668b03e883e3c2101c9226feeb324
Miguel <mra634463@gmail.com>
2016-12-14 12:02:13 -0500

    Delete users controller, add indent, delete unnecessary lines in registrations controller, delete before filter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,74 +1,27 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
-before_filter :configure_account_update_params
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  before_action :configure_account_update_params
 
-  # POST /resource
-  # def create
-  #   super
-  # end
-
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
   protected
 
-  def update_resource(resource, configure_account_update_params)
-    if(configure_account_update_params[:password] != "")
-      resource.password = configure_account_update_params[:password]
-      resource.password_confirmation = configure_account_update_params[:password_confirmation]
+    def update_resource(resource, configure_account_update_params)
+      if(configure_account_update_params[:password] != "")
+        resource.password = configure_account_update_params[:password]
+        resource.password_confirmation = configure_account_update_params[:password_confirmation]
+      end
+      resource.update_without_password(configure_account_update_params)
     end
-    resource.update_without_password(configure_account_update_params)
-  end
 
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
   private
 
-   def configure_account_update_params
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update,
+                                      keys: [:id,
+                                             :full_name,
+                                             :email,
+                                             :telephone,
+                                            :cellphone,
+                                            :address])
+    end
 
-     devise_parameter_sanitizer.permit(:account_update, keys: [:id, :full_name, :email,
-       :telephone,:cellphone, :address])
-   end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
--- spec/support/spec_test_helper.rb
+++ spec/support/spec_test_helper.rb
@@ -8,8 +8,8 @@ module SpecTestHelper
   end
 
   def sign_in(user)
-      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-      allow(controller).to receive(:current_user).and_return(user)
+    allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+    allow(controller).to receive(:current_user).and_return(user)
   end
 
 end
