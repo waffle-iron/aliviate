7548a91413399845a61cedb40d833e129db36ffa
Mauricio Serna Flórez <mauricioserf@gmail.com>
2016-12-17 10:19:43 -0500

    Adding price validation (numericality)

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   # Validations
 
   validates :name, :description, :price, :quantity, presence: true
-  validates :price, :quantity, numericality: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :quantity, numericality: true
 
 end
